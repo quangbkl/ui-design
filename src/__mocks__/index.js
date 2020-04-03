278bc335cca14b20c2e9ab8be02d7b2a3ff5b2ec
anhducc13 <trantienduc10@gmail.com>
2020-04-03 22:51:18 +0700

    hotel item & hotel screen

--- src/__mocks__/index.js
+++ src/__mocks__/index.js
@@ -3,5 +3,6 @@ import mock from './mock';
 import './db/user-db';
 import './db/booking-history-db';
 import './db/post-db';
+import './db/hotel-db';
 
 mock.onAny().passThrough();
--- src/screens/home/Hotels/HotelsScreen.js
+++ src/screens/home/Hotels/HotelsScreen.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, FlatList, StyleSheet} from 'react-native';
+import {v4 as uuidv4 } from 'uuid';
+import {useFilterDynamic} from '../../../hooks/common';
+import {bookingHotel} from '../../../services/bookingServices';
 import FilterSort from '../../../components/FilterSort/FilterSort';
 import CustomHeader from '../../../components/Header/CustomHeader';
 import CustomIcon from '../../../components/Icon/CustomIcon';
@@ -7,27 +10,83 @@ import HotelItem from '../../../components/HotelItem/HotelItem';
 
 // TODO: Code HotelItem component first
 
-const HotelsScreen = () => {
-    const [description, setDescription] = useState("sdfgfd");
-    const renderRightHeader = () => <CustomIcon type="search" />;
+const HotelsScreen = props => {
+  const {startDate, endDate, numRooms} = props;
+  const defaultFilters = {
+    page: 1,
+    limit: 20,
+    startDate,
+    endDate,
+    numRooms,
+  };
+  
+  const [view, setView] = useState('block');
+  const loadDataBookingHotel = params =>
+    bookingHotel(params).then(res => res.data.hotelBooks);
+  const {
+    loading: loadingHotels,
+    list: listHotels,
+    fetchData,
+    refreshPage,
+    fetchNext,
+  } = useFilterDynamic(defaultFilters, loadDataBookingHotel);
 
-    return (
-        <View>
-            <CustomHeader
-                hasLeft
-                hasRight
-                title="Hotels"
-                description={description}
-                right={renderRightHeader()}
-            />
-            <FilterSort />
-            <HotelItem view="block"/>
-            <View style={{marginTop: 20}}/>
-            <HotelItem view="list"/>
-            <View style={{marginTop: 20}}/>
-            <HotelItem view="grid"/>
+  const renderRightHeader = () => <CustomIcon type="search" />;
+  const renderItem = ({item, index}) => {
+    if (view === 'grid' && index % 2 === 1) {
+      return (
+        <View style={[styles.rightItem, styles.eachItem]}>
+          <HotelItem view={view} item={item} />
         </View>
+      );
+    }
+    return (
+      <View style={styles.eachItem}>
+        <HotelItem view={view} item={item} />
+      </View>
     );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  return (
+    <View>
+      <CustomHeader
+        hasLeft
+        hasRight
+        title="Hotels"
+        description="3 April 2020, 2 Nights, 1 Room"
+        right={renderRightHeader()}
+      />
+      <FilterSort view={view} onChangeView={setView} />
+      <FlatList
+        style={view === 'block' ? {} : styles.container}
+        data={listHotels}
+        refreshing={loadingHotels}
+        onRefresh={refreshPage}
+        onEndReached={fetchNext}
+        renderItem={renderItem}
+        key={uuidv4()}
+        keyExtractor={() => uuidv4()}
+        numColumns={view === 'grid' ? 2 : 1}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  eachItem: {
+    marginBottom: 20,
+  },
+  rightItem: {
+    marginLeft: 20,
+  },
+});
 
 export default HotelsScreen;
