6a734f5ec3a1da4e45b41ce12395b585f32634c7
anhducc13 <trantienduc10@gmail.com>
2020-04-05 14:44:51 +0700

    refactor: header

--- src/screens/account/ChangePassword/ChangePasswordScreen.js
+++ src/screens/account/ChangePassword/ChangePasswordScreen.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import { BaseColor } from 'config/color';
-import {Text, CustomHeader, Button } from "components";
+import {BaseColor} from 'config/color';
+import {Text, Header, Button} from 'components';
 import {
     View,
     SafeAreaView,
     ScrollView,
     StyleSheet,
-    TextInput
+    TextInput,
 } from 'react-native';
 
-const ChangePasswordScreen = (props) => {
-   const password = "";
-   const repassword = "";
+const ChangePasswordScreen = () => {
+    const password = '';
+    const repassword = '';
 
     return (
         <SafeAreaView
-            style={{ flex: 1 }}
-            forceInset={{ top: "always" }}
+            style={{flex: 1}}
+            forceInset={{top: 'always'}}
         >
-            <CustomHeader
-                hasLeft
-                title="Change Password"
-            />
+            <Header title="Change Password"/>
             <ScrollView>
                 <View style={styles.contain}>
                     <View style={styles.contentTitle}>
                         <Text headline semibold>
-                                Password
+                            Password
                         </Text>
                     </View>
                     <TextInput
@@ -46,32 +43,32 @@ const ChangePasswordScreen = (props) => {
                     />
                 </View>
             </ScrollView>
-            <View style={{ padding: 20 }}>
+            <View style={{padding: 20}}>
                 <Button>Confirm</Button>
             </View>
         </SafeAreaView>
     );
 };
 
-const styles =  StyleSheet.create({
+const styles = StyleSheet.create({
     contentTitle: {
-        alignItems: "flex-start",
-        width: "100%",
+        alignItems: 'flex-start',
+        width: '100%',
         height: 32,
-        justifyContent: "center"
+        justifyContent: 'center',
     },
     contain: {
-        alignItems: "center",
+        alignItems: 'center',
         padding: 20,
-        width: "100%"
+        width: '100%',
     },
     textInput: {
         height: 46,
         backgroundColor: BaseColor.fieldColor,
         borderRadius: 5,
         padding: 10,
-        width: "100%"
-    }
+        width: '100%',
+    },
 });
 
 export default ChangePasswordScreen;
--- src/screens/account/ProfileInfo/ProfileInfoScreen.js
+++ src/screens/account/ProfileInfo/ProfileInfoScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BaseColor } from 'config/color';
+import {BaseColor} from 'config/color';
 import {
     View,
     SafeAreaView,
     ScrollView,
     StyleSheet,
-    Switch
 } from 'react-native';
 import {
     ProfileDetail,
@@ -13,38 +12,35 @@ import {
     Text,
     CustomIcon,
     Button,
-    CustomHeader,
-    Touchable
-} from "components";
-import appRoutes from '../../../navigations/appRoutes';
+    Header,
+    Touchable,
+} from 'components';
+import appRoutes from 'navigations/appRoutes';
 
 const ProfileInfoScreen = (props) => {
     const {navigation} = props;
     const userData = {
-        image: "https://scontent-xsp1-1.xx.fbcdn.net/v/t1.0-9/80675594_2407151019538149_7255447717231460352_o.jpg?_nc_cat=103&_nc_sid=85a577&_nc_ohc=f8ELipMK0lkAX_Ea6wb&_nc_ht=scontent-xsp1-1.xx&oh=7911fcd87a1d90d45722adf74d30ee97&oe=5EB06405",
-        name: "Phi Xuan Hoan",
-        major: "Travel Company",
-        email: "phihoan10@gmail.com",
-        address: "Ha Noi, Viet Nam",
-        point: "9.5",
-        id: "@hoanpx",
+        image: 'https://scontent-xsp1-1.xx.fbcdn.net/v/t1.0-9/80675594_2407151019538149_7255447717231460352_o.jpg?_nc_cat=103&_nc_sid=85a577&_nc_ohc=f8ELipMK0lkAX_Ea6wb&_nc_ht=scontent-xsp1-1.xx&oh=7911fcd87a1d90d45722adf74d30ee97&oe=5EB06405',
+        name: 'Phi Xuan Hoan',
+        major: 'Travel Company',
+        email: 'phihoan10@gmail.com',
+        address: 'Ha Noi, Viet Nam',
+        point: '9.5',
+        id: '@hoanpx',
         about:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
         performance: [
-            { value: "92.1%", title: "Feedback" },
-            { value: "99", title: "Items" },
-            { value: "10k", title: "Followers" }
-        ]
-    }
+            {value: '92.1%', title: 'Feedback'},
+            {value: '99', title: 'Items'},
+            {value: '10k', title: 'Followers'},
+        ],
+    };
 
     return (
         <SafeAreaView
-            style={{ flex: 1 }}
-            forceInset={{ top: "always" }}>
-            <CustomHeader
-                hasLeft
-                title="Profile"
-            />
+            style={{flex: 1}}
+            forceInset={{top: 'always'}}>
+            <Header title="Profile"/>
             <ScrollView>
                 <View style={styles.contain}>
                     <ProfileDetail
@@ -54,14 +50,14 @@ const ProfileInfoScreen = (props) => {
                         textSecond={userData.address}
                         textThird={userData.id}
                         onPress={() =>
-                            navigation.navigate("")
+                            navigation.navigate('')
                         }
                     />
                     <ProfilePerformance
                         data={userData.performance}
                         style={styles.performanceContainer}
                     />
-                    <View style={{ width: "100%" }}>
+                    <View style={{width: '100%'}}>
                         <Touchable
                             style={styles.profileItem}
                             onPress={() => {
@@ -73,7 +69,7 @@ const ProfileInfoScreen = (props) => {
                                 name="angle-right"
                                 size={18}
                                 color={BaseColor.primaryColor}
-                                style={{ marginLeft: 5 }}
+                                style={{marginLeft: 5}}
                             />
                         </Touchable>
                         <Touchable
@@ -87,21 +83,21 @@ const ProfileInfoScreen = (props) => {
                                 name="angle-right"
                                 size={18}
                                 color={BaseColor.primaryColor}
-                                style={{ marginLeft: 5 }}
+                                style={{marginLeft: 5}}
                             />
                         </Touchable>
-                        
+
                         <Touchable
                             style={styles.profileItem}
                             onPress={() => {
-                                navigation.navigate("");
+                                navigation.navigate('');
                             }}
                         >
                             <Text body1>Currency</Text>
                             <View
                                 style={{
-                                    flexDirection: "row",
-                                    alignItems: "center"
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
                                 }}
                             >
                                 <Text body1 grayColor>
@@ -111,14 +107,14 @@ const ProfileInfoScreen = (props) => {
                                     name="angle-right"
                                     size={18}
                                     color={BaseColor.primaryColor}
-                                    style={{ marginLeft: 5 }}
+                                    style={{marginLeft: 5}}
                                 />
                             </View>
                         </Touchable>
                         <Touchable
                             style={styles.profileItem}
                             onPress={() => {
-                                navigation.navigate("");
+                                navigation.navigate('');
                             }}
                         >
                             <Text body1>My Cart</Text>
@@ -126,7 +122,7 @@ const ProfileInfoScreen = (props) => {
                                 name="angle-right"
                                 size={18}
                                 color={BaseColor.primaryColor}
-                                style={{ marginLeft: 5 }}
+                                style={{marginLeft: 5}}
                             />
                         </Touchable>
                         <Touchable
@@ -140,13 +136,13 @@ const ProfileInfoScreen = (props) => {
                                 name="angle-right"
                                 size={18}
                                 color={BaseColor.primaryColor}
-                                style={{ marginLeft: 5 }}
+                                style={{marginLeft: 5}}
                             />
                         </Touchable>
                     </View>
                 </View>
             </ScrollView>
-            <View style={{ padding: 20 }}>
+            <View style={{padding: 20}}>
                 <Button>
                     Sign Out
                 </Button>
@@ -157,30 +153,30 @@ const ProfileInfoScreen = (props) => {
 
 const styles = StyleSheet.create({
     performanceContainer: {
-        backgroundColor: "#E8E8E8",
+        backgroundColor: '#E8E8E8',
         marginTop: 20,
-        marginBottom: 20
+        marginBottom: 20,
     },
     contentTitle: {
-        alignItems: "flex-start",
-        width: "100%",
+        alignItems: 'flex-start',
+        width: '100%',
         height: 32,
-        justifyContent: "center"
+        justifyContent: 'center',
     },
     contain: {
-        alignItems: "center",
+        alignItems: 'center',
         padding: 20,
-        width: "100%"
+        width: '100%',
     },
     profileItem: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
         borderBottomColor: BaseColor.textSecondaryColor,
         borderBottomWidth: 1,
         paddingBottom: 20,
-        paddingTop: 20
-    }
+        paddingTop: 20,
+    },
 });
 
 export default ProfileInfoScreen;
