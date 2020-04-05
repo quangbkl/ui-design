9feca31cf0287d1f3e852acf09a0c2f092846c19
hoanphi2201 <phihoan10@gmail.com>
2020-04-05 17:34:48 +0700

    Text Default

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -50,57 +50,58 @@ const Text = (props) => {
     return (
         <ReactText
             style={StyleSheet.flatten([
+                styles.textDefault,
                 header && Typography.header,
-                    title1 && Typography.title1,
-                    title2 && Typography.title2,
-                    title3 && Typography.title3,
-                    headline && Typography.headline,
-                    body1 && Typography.body1,
-                    body2 && Typography.body2,
-                    callout && Typography.callout,
-                    subhead && Typography.subhead,
-                    footnote && Typography.footnote,
-                    caption1 && Typography.caption1,
-                    caption2 && Typography.caption2,
-                    overline && Typography.overline,
-                    //custom for font
-                    thin && StyleSheet.flatten({ fontWeight: FontWeight.thin }),
-                    ultraLight &&
-                        StyleSheet.flatten({
-                            fontWeight: FontWeight.ultraLight
-                        }),
-                    light && StyleSheet.flatten({ fontWeight: FontWeight.light }),
-                    regular && StyleSheet.flatten({ fontWeight: FontWeight.regular }),
-                    medium && StyleSheet.flatten({ fontWeight: FontWeight.medium }),
-                    semibold && StyleSheet.flatten({ fontWeight: FontWeight.semibold }),
-                    bold && StyleSheet.flatten({ fontWeight: FontWeight.bold }),
-                    heavy && StyleSheet.flatten({ fontWeight: FontWeight.heavy }),
-                    black && StyleSheet.flatten({ fontWeight: FontWeight.black }),
-                    // default color
+                title1 && Typography.title1,
+                title2 && Typography.title2,
+                title3 && Typography.title3,
+                headline && Typography.headline,
+                body1 && Typography.body1,
+                body2 && Typography.body2,
+                callout && Typography.callout,
+                subhead && Typography.subhead,
+                footnote && Typography.footnote,
+                caption1 && Typography.caption1,
+                caption2 && Typography.caption2,
+                overline && Typography.overline,
+                //custom for font
+                thin && StyleSheet.flatten({ fontWeight: FontWeight.thin }),
+                ultraLight &&
                     StyleSheet.flatten({
-                        color: BaseColor.textPrimaryColor
+                        fontWeight: FontWeight.ultraLight
                     }),
-                    //custom for color
-                    primaryColor && StyleSheet.flatten({ color: BaseColor.primaryColor }),
-                    darkPrimaryColor &&
-                        StyleSheet.flatten({
-                            color: BaseColor.darkPrimaryColor
-                        }),
-                    lightPrimaryColor &&
-                        StyleSheet.flatten({
-                            color: BaseColor.lightPrimaryColor
-                        }),
-                    accentColor && StyleSheet.flatten({ color: BaseColor.accentColor }),
-                    textSecondaryColor &&
-                        StyleSheet.flatten({
-                            color: BaseColor.textSecondaryColor
-                        }),
-                    grayColor && StyleSheet.flatten({ color: BaseColor.grayColor }),
-                    darkBlueColor && StyleSheet.flatten({ color: BaseColor.darkBlueColor }),
-                    dividerColor && StyleSheet.flatten({ color: BaseColor.dividerColor }),
-                    whiteColor && StyleSheet.flatten({ color: BaseColor.whiteColor }),
-                    fieldColor && StyleSheet.flatten({ color: BaseColor.fieldColor }),
-                    style && style
+                light && StyleSheet.flatten({ fontWeight: FontWeight.light }),
+                regular && StyleSheet.flatten({ fontWeight: FontWeight.regular }),
+                medium && StyleSheet.flatten({ fontWeight: FontWeight.medium }),
+                semibold && StyleSheet.flatten({ fontWeight: FontWeight.semibold }),
+                bold && StyleSheet.flatten({ fontWeight: FontWeight.bold }),
+                heavy && StyleSheet.flatten({ fontWeight: FontWeight.heavy }),
+                black && StyleSheet.flatten({ fontWeight: FontWeight.black }),
+                // default color
+                StyleSheet.flatten({
+                    color: BaseColor.textPrimaryColor
+                }),
+                //custom for color
+                primaryColor && StyleSheet.flatten({ color: BaseColor.primaryColor }),
+                darkPrimaryColor &&
+                    StyleSheet.flatten({
+                        color: BaseColor.darkPrimaryColor
+                    }),
+                lightPrimaryColor &&
+                    StyleSheet.flatten({
+                        color: BaseColor.lightPrimaryColor
+                    }),
+                accentColor && StyleSheet.flatten({ color: BaseColor.accentColor }),
+                textSecondaryColor &&
+                    StyleSheet.flatten({
+                        color: BaseColor.textSecondaryColor
+                    }),
+                grayColor && StyleSheet.flatten({ color: BaseColor.grayColor }),
+                darkBlueColor && StyleSheet.flatten({ color: BaseColor.darkBlueColor }),
+                dividerColor && StyleSheet.flatten({ color: BaseColor.dividerColor }),
+                whiteColor && StyleSheet.flatten({ color: BaseColor.whiteColor }),
+                fieldColor && StyleSheet.flatten({ color: BaseColor.fieldColor }),
+                style && style
             ])}
             numberOfLines={numberOfLines}
             {...rest}
@@ -111,5 +112,13 @@ const Text = (props) => {
 Text.propTypes = {
     style: ReactText.propTypes.style,
 };
+
+const styles = StyleSheet.create({  
+    textDefault: {
+        ...Typography.headline, // Typography > headline
+        color: BaseColor.whiteColor,
+        fontWeight: FontWeight.semibold
+    }
+});
 
 export default Text;
